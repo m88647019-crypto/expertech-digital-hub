fd741b08a084ab2f06637fffde668feed10ef76d
m88647019-crypto <m88647019@gmail.com>
2026-03-14 21:59:05 +0300

    Create stkPush.js

---
+++ netlify/functions/stkPush.js
@@ -0,0 +1,56 @@
+exports.handler = async (event) => {
+
+const { phone, amount } = JSON.parse(event.body)
+
+const tokenResponse = await fetch(
+"https://expertechcyberonline.netlify.app/.netlify/functions/getAccessToken"
+)
+
+const tokenData = await tokenResponse.json()
+
+const accessToken = tokenData.access_token
+
+const shortcode = process.env.MPESA_SHORTCODE
+const passkey = process.env.MPESA_PASSKEY
+
+const timestamp = new Date()
+.toISOString()
+.replace(/[-:.TZ]/g,"")
+.slice(0,14)
+
+const password = Buffer.from(
+shortcode + passkey + timestamp
+).toString("base64")
+
+const stkResponse = await fetch(
+"https://sandbox.safaricom.co.k/mpesa/stkpush/v1/processrequest",
+{
+method: "POST",
+headers: {
+Authorization: `Bearer ${accessToken}`,
+"Content-Type": "application/json"
+},
+body: JSON.stringify({
+BusinessShortCode: shortcode,
+Password: password,
+Timestamp: timestamp,
+TransactionType: "CustomerPayBillOnline",
+Amount: amount,
+PartyA: phone,
+PartyB: shortcode,
+PhoneNumber: phone,
+CallBackURL: "https://expertechcyberonline/mpesa-callback",
+AccountReference: "ExpertechPrint",
+TransactionDesc: "Print Order"
+})
+}
+)
+
+const result = await stkResponse.json()
+
+return {
+statusCode: 200,
+body: JSON.stringify(result)
+}
+
+}
