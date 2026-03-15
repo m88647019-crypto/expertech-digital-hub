4fce8dcdffcbfac7fb10a9d00813c229c297a064
m88647019-crypto <m88647019@gmail.com>
2026-03-15 15:26:52 +0300

    Update getAccessToken.js

--- netlify/functions/getAccessToken.js
+++ netlify/functions/getAccessToken.js
@@ -1,25 +1,35 @@
 exports.handler = async () => {
 
-const consumerKey = process.env.MPESA_CONSUMER_KEY
-const consumerSecret = process.env.MPESA_CONSUMER_SECRET
+  const consumerKey = process.env.MPESA_CONSUMER_KEY
+  const consumerSecret = process.env.MPESA_CONSUMER_SECRET
 
-const auth = Buffer.from(`${consumerKey}:${consumerSecret}`).toString("base64")
+  const auth = Buffer.from(`${consumerKey}:${consumerSecret}`).toString("base64")
 
-const response = await fetch(
-"https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials",
-{
-method: "GET",
-headers: {
-Authorization: `Basic ${auth}`
-}
-}
-)
+  try {
 
-const data = await response.json()
+    const response = await fetch(
+      "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials",
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Basic ${auth}`
+        }
+      }
+    )
 
-return {
-statusCode: 200,
-body: JSON.stringify(data)
-}
+    const data = await response.json()
 
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data)
+    }
+
+  } catch (error) {
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message })
+    }
+
+  }
 }
